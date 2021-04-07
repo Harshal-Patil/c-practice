bc6f6ab62228e6b22347d4ba408e7276def2ab85
harshal patil <harshpatilheros@gmail.com>
2021-04-07 18:21:49 +0530

    Finding Sum of First N natural Number

---
+++ sumofnum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,sum;
+	cout<<"Enter Number=";
+	cin>>n;
+	sum=n*(n+1)/2;
+	cout<<"Sum of first natural Numbers="<<sum<<endl;
+	return 0;
+}
